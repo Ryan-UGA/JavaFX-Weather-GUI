451642cda8ca06278a4daee5e66e0a54428e2410
Ryan Cullen <rdc35950@uga.edu>
2023-05-01 14:41:52 -0400

    base established

--- src/main/java/cs1302/api/ApiApp.java
+++ src/main/java/cs1302/api/ApiApp.java
@@ -8,14 +8,34 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * REPLACE WITH NON-SHOUTING DESCRIPTION OF YOUR APP.
  */
 public class ApiApp extends Application {
+    /** This will search the seafood category. */
+    private static final String URL = "www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
+    /** HTTP client. */
+    public static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+        .version(HttpClient.Version.HTTP_2)           // uses HTTP protocol version 2 where possible
+        .followRedirects(HttpClient.Redirect.NORMAL)  // always redirects, except from HTTPS to HTTP
+        .build();                                     // builds and returns a HttpClient object
+    /** Google {@code Gson} object for parsing JSON-formatted strings. */
+    public static Gson GSON = new GsonBuilder()
+        .setPrettyPrinting()                          // enable nice output when printing
+        .create();                                    // builds and returns a Gson object
+    /** Variables used below. */
     Stage stage;
     Scene scene;
     VBox root;
+    Button button;
 
     /**
      * Constructs an {@code ApiApp} object. This default (i.e., no argument)
@@ -23,18 +43,20 @@ public class ApiApp extends Application {
      */
     public ApiApp() {
         root = new VBox();
+        button = new Button("Click this");
     } // ApiApp
 
     /** {@inheritDoc} */
     public void init() {
-        Image bannerImage = new Image("file:resources/readme-banner.png");
+        /**Image bannerImage = new Image("file:resources/readme-banner.png");
         ImageView banner = new ImageView(bannerImage);
         banner.setPreserveRatio(true);
         banner.setFitWidth(640);
         // some labels to display information
         Label notice = new Label("Modify the starter code to suit your needs.");
         // setup scene
-        root.getChildren().addAll(banner, notice);
+        root.getChildren().addAll(banner, notice); */
+        root.getChildren().addAll(button);
     } // init
 
     /** {@inheritDoc} */
